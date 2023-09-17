719a3ba32592a8aa09d374ca42b1546a108bbfc9
j <77560326+Jxl-s@users.noreply.github.com>
2023-09-17 14:31:47 -0400

    feat: added slim template

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ .'/app/Config/app_config.php';
+
+// Step 1) Instantiate a Slim app.
+$app = AppFactory::create();
+
+// Add the routing and body parsing middleware.
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+
+// NOTE: the error handling middleware MUST be added last.
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->getDefaultErrorHandler()->forceContentType(APP_MEDIA_TYPE_JSON);
+
+$app->setBasePath("/crimes-api");
+
+// Here we include the file that contains the application routes. 
+// NOTE: your routes must be managed in the api_routes.php file.
+require_once __DIR__ . '/app/Routes/app_routes.php';
+
+// Run the app.
+$app->run();
