bae61cf76d9ad847d17651e5fb1b6b6cf8c12b8d
Jia Xuan Li <2154201@edu.vaniercollege.qc.ca>
2023-11-14 09:21:03 -0500

    added token/account

--- app/Controllers/AccountsController.php
+++ app/Controllers/AccountsController.php
@@ -30,14 +30,20 @@ class AccountsController extends BaseController
         if (empty($account_data)) {
             return $this->prepareOkResponse($response, ['error' => true, 'message' => 'No data was provided in the request.'], 400);
         }
-        //TODO: before creating the account, verify if there is already an existing one with the provided email.
-        // 2) Data was provided, we attempt to create an account for the user.                
 
-        
-        //if (!$new_account_id) {
+        // Before creating the account, verify if there is already an existing one with the provided email.
+        if ($this->accounts_model->isAccountExist($account_data['email'])) {
+            return $this->prepareOkResponse($response, ['error' => true, 'message' => 'Email already taken'], 400);
+        }
+
+        // 2) Data was provided, we attempt to create an account for the user.                
+        $new_account_id = $this->accounts_model->createAccount($account_data);
+
+        if (!$new_account_id) {
             // 2.a) Failed to create the new account.
-        //}
-        
+            return $this->prepareOkResponse($response, ['error' => true, 'message' => 'Failed to create new account'], 400);
+        }
+
         // 3) A new account has been successfully created. 
         // Prepare and return a response.  
         return $response;
@@ -49,19 +55,20 @@ class AccountsController extends BaseController
         //var_dump($user_data);exit;
 
         //-- 1) Reject the request if the request body is empty.
+        if (empty($account_data)) {
+            return $this->prepareOkResponse($response, ['error' => true, 'message' => 'No data was provided in the request.'], 400);
+        }
 
         //-- 2) Retrieve and validate the account credentials.
+        $db_account = $this->accounts_model->isPasswordValid($account_data['email'], $account_data['password']);
 
         //-- 3) Is there an account matching the provided email address in the DB?
-
         //-- 4) If so, verify whether the provided password is valid.
-
-        
-        //if (!$db_account) {
-            //-- 4.a) If the password is invalid --> prepare and return a response with a message indicating the 
-            // reason.            
-        //}
-        
+        //-- 4.a) If the password is invalid --> prepare and return a response with a message indicating the 
+        // reason.   
+        if (!$db_account) {
+            return $this->prepareOkResponse($response, ['error' => true, 'message' => 'Invalid credentials'], 401);
+        }
 
         //-- 5) Valid account detected => Now, we return an HTTP response containing
         // the newly generated JWT.
@@ -72,9 +79,13 @@ class AccountsController extends BaseController
         $expires_in = time() + 60; //! NOTE: Expires in 1 minute.
         //!note: the time() function returns the current timestamp, which is the number of seconds since January 1st, 1970
         //-- 5.b) Create a JWT using the JWTManager's generateJWT() method.
-        //$jwt = JWTManager::generateJWT($account_data, $expires_in);
-        //--
+        $jwt = JWTManager::generateJWT([
+            'user_id' => $db_account['user_id'],
+            'email' => $db_account['email'],
+            'role' => $db_account['role'],
+        ], $expires_in);
+
         // 5.c) Prepare and return a response with a JSON doc containing the jwt.
-        return $response;
+        return $this->prepareOkResponse($response, ['status' => "success", 'token' => $jwt, "message" => 'Successfully logged in!'], 201);
     }
 }
