15e6a9459e7d11126a381aa7e25093ce1abae7b8
Jia Xuan Li <2154201@edu.vaniercollege.qc.ca>
2023-11-17 13:15:17 -0500

    fix db

--- data/crimes_db.sql
+++ data/crimes_db.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `crimes_db`
 --
-
+CREATE DATABASE IF NOT EXISTS `crimes_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `crimes_db`;
 -- --------------------------------------------------------
 
 --
